fb0ef55a20c92705e838397ae82fce03e3e569a6
Kajofzo <108760962+Kajofzo@users.noreply.github.com>
2023-12-06 11:29:35 +0100

    day1 part 2 done

--- 2023/day1part2/script.js
+++ 2023/day1part2/script.js
@@ -4,17 +4,10 @@ fetch("./input.txt").then((r) => r.text()).then((input) => {
     for (let i = 0; i < lines.length; i++) {
         let start = 0;
         let end = 0;
-        let replacenum1 = lines[i].replace("one", "1");
-        let replacenum2 = replacenum1.replace("two", "2");
-        let replacenum3 = replacenum2.replace("three", "3");
-        let replacenum4 = replacenum3.replace("four", "4");
-        let replacenum5 = replacenum4.replace("five", "5");
-        let replacenum6 = replacenum5.replace("six", "6");
-        let replacenum7 = replacenum6.replace("seven", "7");
-        let replacenum8 = replacenum7.replace("eight", "8");
-        let replacenumfinal = replacenum8.replace("nine", "9");
         
-        for (let y = 0; y < replacenumfinal.length; y++) {
+        lines[i] = lines[i].replaceAll("one", "o1e").replaceAll("two", "t2o").replaceAll("three", "t3e").replaceAll("four", "f4r").replaceAll("five", "f5e").replaceAll("six", "s6x").replaceAll("seven", "s7n").replaceAll("eight", "e8t").replaceAll("nine", "n9e");
+        
+        for (let y = 0; y < lines[i].length; y++) {
             if (numbercheck(lines[i][y])) {
                 end = lines[i][y];
                 if (start == 0) {
